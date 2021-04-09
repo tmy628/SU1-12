eecf6aa8b675594cab69a9ead76c9da79ff46cb6
tmy628 <tmy6h1r@gmail.com>
2021-04-09 08:27:21 +0000

    [Finish] SU1-12

--- sample_app/db/seeds.rb
+++ sample_app/db/seeds.rb
@@ -10,8 +10,9 @@
     title: 'railsを学ぼう',
     body: 'seedファイルについて'
   )
-# 初期データの記述
+  # 初期データの記述
 
   10.times do |number|
     List.create!(title: 'timesを学習しよう',body: number)
   end
+  # timesメソッドを用いて、seedファイルでは同時に複数のデータを作成する事ができる
